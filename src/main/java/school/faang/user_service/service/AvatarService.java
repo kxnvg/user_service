f57dd76cb2c07e441214ba8dbd1ec24a98ba1ea7
svetlana.obrancova <obrancova.s@gmail.com>
2023-08-16 20:14:56 +0200

    BC-4271-1 created AvatarService

---
+++ src/main/java/school/faang/user_service/service/AvatarService.java
@@ -0,0 +1,10 @@
+package school.faang.user_service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AvatarService {
+
+}
